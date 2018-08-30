eae5330e4f86c681262785a44014565d887872f7
nguyenthanhtunghframgia <nguyen.thanh.tungh@framgia.com>
2018-08-30 18:20:08 +0700

    init_mainscreen

---
+++ app/src/main/java/com/framgia/music_28/screen/main/fragment/AllTrackFragment.java
@@ -0,0 +1,19 @@
+package com.framgia.music_28.screen.main.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.framgia.music_28.R;
+
+public class AllTrackFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.all_track_fragment, container, false);
+    }
+}
